4a0114172caec3b3ac2f4ed8528330605f87c761
Lucas Prado <lucas.rodrigues.prado120@gmail.com>
2023-07-22 18:43:32 -0300

    add api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL:"https://food-explorer-api-lucas.onrender.com",
- baseURL:"http://localhost:3333",
-  
+  baseURL:"https://food-explorer-api-lucas.onrender.com",
+
 })
